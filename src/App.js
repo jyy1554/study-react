c2797bc3e7b0461736ac78caa19aa870a28fc14a
jyy1554 <jyy1556@gmail.com>
2021-11-14 00:12:11 +0900

    클래스형 컴포넌트 사용법

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() { 
-  const name = '리액트';
-  return (
-    <>
-      {/* 주석은 이렇게 작성합니다.*/}
-      <div 
-        className="react" //시작 태그를 여러줄로 작성하면 이렇게 주석을 달 수 있따
-      >
-        {name}
-      </div>
-      // 하지만 이런 주석이나
-      /* 이런 주석은 페이지에 그대로 나타나게 됨 */
-      <input />
-    </>
-  );
+class App extends Component {
+  render() {
+    const name = 'react';
+    return <div className="react">{name}</div>;
+  }
 }
 
 export default App;
