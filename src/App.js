f5ae45d935a72a090d9ecc3962f1de569a639910
jyy1554 <jyy1556@gmail.com>
2021-11-13 23:29:39 +0900

    주석은 {/* ... */} 이런식으로 달아야 함

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import './App.css'; //추가
+import './App.css';
 
 function App() { 
   const name = '리액트';
   return (
     <>
-      <div className="react">{name}</div>
+      {/* 주석은 이렇게 작성합니다.*/}
+      <div 
+        className="react" //시작 태그를 여러줄로 작성하면 이렇게 주석을 달 수 있따
+      >
+        {name}
+      </div>
+      // 하지만 이런 주석이나
+      /* 이런 주석은 페이지에 그대로 나타나게 됨 */
       <input />
     </>
   );
