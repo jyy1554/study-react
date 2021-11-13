2ee256583e109fe27f4f177f52e0945157f506d8
jyy1554 <jyy1556@gmail.com>
2021-11-13 23:16:39 +0900

    인라인 스타일링

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() {  // 함수형 컴포넌트 (HTML도 문자열 템플릿도 아니며 JSX라 함)
+  const name = '리액트';
+  const style = {
+    //background-color는 backgroundColor와 같이 -가 사라지고 카멜 표기법으로 작성해야함
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: '48px', // font-size -> fontSize
+    fontWeight: 'bold', //font-weight -> fontWeight
+    padding: 16 // 단위 생략하면 px로 지정됨
+  };
+
+  return <div style={style}>{name}</div>;
 }
 
 export default App;
