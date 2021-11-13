152cf956a6fbcfeb3c14dcca1f6be0867a364745
jyy1554 <jyy1556@gmail.com>
2021-11-13 23:21:46 +0900

    JSX에서 class 대신 className으로 설정해주기

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from 'react';
+import './App.css'; //추가
 
-function App() {  // 함수형 컴포넌트 (HTML도 문자열 템플릿도 아니며 JSX라 함)
+function App() { 
   const name = '리액트';
-  const style = {
-    //background-color는 backgroundColor와 같이 -가 사라지고 카멜 표기법으로 작성해야함
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: '48px', // font-size -> fontSize
-    fontWeight: 'bold', //font-weight -> fontWeight
-    padding: 16 // 단위 생략하면 px로 지정됨
-  };
-
-  return <div style={style}>{name}</div>;
+  return <div className="react">{name}</div>;
 }
 
 export default App;
