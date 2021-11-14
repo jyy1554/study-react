3e7806996ef7525dcd3eb0299466e3ba198389c0
jyy1554 <jyy1556@gmail.com>
2021-11-14 18:35:55 +0900

    Input 여러개 다루기

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -3,26 +3,21 @@ import React, { Component } from 'react';
 class EventPractice extends Component {
 
   state = {
+    username: '',
     message: ''
   }
 
-  constructor(props) {
-    super(props);
-    // 임의 메서드가 이벤트로 등록되어도 this를 컴포넌트 자신으로 제대로 가리키기 위해 메서드를 this와 바인딩하는 작업이 필요
-    // 바인딩하지 않으면 this가 undefined를 가리킴
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleChange(e) {
+  handleChange = (e) => {
     this.setState({
-      message: e.target.value
+      //객체 안에서 key를 []로 감싸면, 그 안에 넣은 레퍼런스가 가리키는 실제 값이 key값으로 사용됨
+      [e.target.name]: e.target.value
     });
   }
 
-  handleClick() {
-    alert(this.state.message);
+  handleClick = () => {
+    alert(this.state.username + ': ' + this.state.message);
     this.setState({
+      username: '',
       message: ''
     });
   }
@@ -31,6 +26,13 @@ class EventPractice extends Component {
     return (
       <div>
         <h1>이벤트 연습</h1>
+        <input
+          type="text"
+          name="username"
+          placeholder="사용자명"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
         <input
           type="text"
           name="message"
@@ -44,4 +46,4 @@ class EventPractice extends Component {
   }
 }
 
-export default EventPractice;<h1>이벤트 연습</h1>
+export default EventPractice;
