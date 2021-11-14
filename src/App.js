b0688ed6f399f6c8c7c48a5270e0d5aee40ba6c4
jyy1554 <jyy1556@gmail.com>
2021-11-14 17:13:33 +0900

    클래스형 컴포넌트의 state 작성해보기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import MyComponent from './MyComponent';
+import Counter from './Counter';
 
 const App = () => {
-  return <MyComponent name="React" favoriteNumber={7}>리액트</MyComponent>;
+  return <Counter />;
 };
 
 export default App;
