06d0d1fa81e224f74baf68ecdeb1cde6c5aa4aac
jyy1554 <jyy1556@gmail.com>
2021-11-15 23:01:20 +0900

    함수형 컴포넌트에서 useReducer 사용하기

--- src/Info.js
+++ src/Info.js
@@ -1,30 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer } from 'react';
+
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value
+  };
+}
 
 const Info = () => {
-  const [name, setName] = useState('');
-  const [nickname, setNickname] = useState('');
-  useEffect(() => {
-    console.log('effect');
-    console.log(name);
-    return () => {
-      console.log('cleanup');
-      console.log(name);
-    }
-  }, []);
+  const [state, dispatch] = useReducer(reducer, {
+    name: '',
+    nickname: ''
+  });
+  const {name, nickname} = state;
 
-  const onChangeName = e => {
-    setName(e.target.value);
-  };
-
-  const onChangeNickname = e => {
-    setNickname(e.target.value);
+  const onChange = e => { //인풋 개수가 많아져도 코드를 짧고 깔끔하게 유지 가능
+    dispatch(e.target);
   };
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
