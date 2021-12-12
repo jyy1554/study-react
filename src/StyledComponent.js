05eae403de2ac9a0e9de75a9ed335b2721f1a8b3
jyy1554 <jyy1556@gmail.com>
2021-12-12 14:25:36 +0900

    반응형 디자인 - media query를 유틸 함수 이용해서 사용해보기

--- src/StyledComponent.js
+++ src/StyledComponent.js
@@ -3,6 +3,22 @@ import styled, {css} from 'styled-components';
 /* 단순 변수의 형태가 아니라 여러 줄의 스타일 구문을 조건부로 설정해야 하는 경우에는 css를 불러와야 함 */
 
 
+const sizes = {
+  desktop: 1024,
+  tablet: 768
+};
+
+//위에 있는 size 객체에 따라 자동으로 media 쿼리 함수를 만들어줌
+const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label] / 16}em) {
+      ${css(...args)};
+    }
+  `;
+
+  return acc;
+}, {});
+
 const Box = styled.div`
   /* props로 넣어 준 값을 직접 전달해 줄 수 있음*/
   background: ${props => props.color || 'blue'};
@@ -12,12 +28,8 @@ const Box = styled.div`
     가로 크기가 작아짐에 따라 크기를 줄이고 768px 미만이 되면 꽉 채움 */
   width: 1024px;
   margin: 0 auto;
-  @media (max-width: 1024px) {
-    width: 768px;
-  }
-  @media (max-width: 768px) {
-    width: 100%;
-  }
+  ${media.desktop`width: 768px;`}
+  ${media.tablet`width: 100%;`};
 `;
 
 const Button = styled.button`
