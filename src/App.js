8333b177bf4ba63ef5fa760dd1b66eda7e7ecb2c
jyy1554 <jyy1556@gmail.com>
2022-02-06 00:16:18 +0900

    forEach, map 사용하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import ArrowFunc from './components/R013_ArrowFunction';
+import Map from './components/R015_Map';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ArrowFunc/>
+      <Map/>
     </div>
   );
 }
