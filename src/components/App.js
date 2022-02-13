29bd86ec2fe736fa84d76f64100d2474ec72d2b4
jyy1554 <jyy1556@gmail.com>
2022-02-13 23:31:09 +0900

    axios로 api 호출하기

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,15 @@ import '../css/new.css';
 import HeaderAdmin from './Header/HeaderAdmin';
 import Footer from './Footer/Footer';
 import LoginForm from './LoginForm';
+import FloatingPopulationList from './Floating_population/floatingPopulationList';
 
 const App = () => {
   return (
     <div className="App">
       <HeaderAdmin/>
       <Routes>
-        <Route path="/" element={<LoginForm />} />
+        <Route path="/" exact element={<LoginForm />} />
+        <Route path='/floatPopulationList' exact element={<FloatingPopulationList />} />
       </Routes>
       <Footer/>
     </div>
