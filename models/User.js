bad82be83c952e4d1d81224619b2cb128e556e2c
jyy1554 <jyy1556@gmail.com>
2025-03-03 12:57:54 +0900

    feat: add logout router with node.js

--- models/User.js
+++ models/User.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
+const jwt = require('jsonwebtoken');
+
+
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -50,8 +53,48 @@ userSchema.pre('save', function (next) { // save하기전에 해라
   } else {
     next();
   }
-  
 })
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+
+  // plainPassword 1234567      암호화된 비밀번호 $2b$10$DY9FhXG/DUbunaMG02K79uQ4
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+}
+
+userSchema.methods.generateToken = function (cb) {
+
+  var user = this;
+
+  // jsonwebtoken을 이용해서 token을 생성하기
+  var token = jwt.sign(user._id.toHexString(), 'secretToken');
+  // user._id + 'secretToken' => token 생성
+  // 'secretToken' => id 알아낼 수 있음
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+  var user = this;
+
+  // 토큰을 decode 한다.
+  jwt.verify(token, 'secretToken', function(err, decoded) {
+    // 유저 아이디를 이용해 유저를 찾은 다음에
+    // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+
+    user.findOne({"_id": decoded, "token": token}, function(err, user) {
+      if(err) return cb(err);
+      cb(null, user);
+    });
+  });
+
+};
 
 const User = mongoose.model('User', userSchema);
 
