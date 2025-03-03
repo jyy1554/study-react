4e25d65732f9bbc2e10972c28c6061d57f40230d
jyy1554 <jyy1556@gmail.com>
2025-03-03 22:14:37 +0900

    feat: check authentication to enter specific page

--- client/src/App.js
+++ client/src/App.js
@@ -9,26 +9,24 @@ import {
 import LandingPage from './components/views/LandingPage/LandingPage';
 import LoginPage from './components/views/LoginPage/LoginPage';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
+import Auth from './hoc/auth';
 
 function App() {
+  const AuthLandingPage = Auth(LandingPage, null);
+  const AuthLoginPage = Auth(LoginPage, false);
+  const AuthRegisterPage = Auth(RegisterPage, false);
+
   return (
     <Router>
       <div>
-        {/*
-          A <Routes> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Routes> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
         <Routes>
-          <Route exact path="/" element={<LandingPage />} />
-          <Route exact path="/login" element={<LoginPage />} />
-          <Route exact path="/register" element={<RegisterPage />} />
+          <Route exact path="/" element={<AuthLandingPage />} />
+          <Route exact path="/login" element={<AuthLoginPage />} />
+          <Route exact path="/register" element={<AuthRegisterPage />} />
         </Routes>
       </div>
     </Router>
-  )
+  );
 }
 
 export default App;
