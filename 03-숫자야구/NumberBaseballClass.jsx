b2937ab7f368d4d33ec296a2f155efdbf066194e
jyy1554 <jyy1556@gmail.com>
2025-03-09 17:47:29 +0900

    fix: add prevState in setState with spread operator

--- 03-숫자야구/NumberBaseballClass.jsx
+++ 03-숫자야구/NumberBaseballClass.jsx
@@ -34,9 +34,11 @@ class NumberBaseballClass extends Component {
     const { value, answer, tries } = this.state;
 
     if (value === answer.join('')) {
-      this.setState({
-        result: '홈런!',
-        tries: [...tries, { try: value, result: '홈런!' }],
+      this.setState((prevState) => {
+        return {
+          result: '홈런!',
+          tries: [...tries, { try: value, result: '홈런!' }],
+        };
       });
       alert('게임을 다시 시작합니다.');
       this.setState({
@@ -68,9 +70,11 @@ class NumberBaseballClass extends Component {
           }
         }
 
-        this.setState({
-          tries: [...tries, { try: value, result: `${strike} 스트라이크, ${ball} 볼` }],
-          value: '',
+        this.setState((prevState) => {
+          return {
+            tries: [...tries, { try: value, result: `${strike} 스트라이크, ${ball} 볼` }],
+            value: '',
+          };
         });
       }
     }
