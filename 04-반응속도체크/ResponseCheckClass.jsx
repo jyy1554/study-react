c1c8e0c2979dd8e7b0b968af3f0e9a5f978daa94
jyy1554 <jyy1556@gmail.com>
2025-03-09 22:24:29 +0900

    refactor: separate css and add clearTimeout, ternary operator

--- 04-반응속도체크/ResponseCheckClass.jsx
+++ 04-반응속도체크/ResponseCheckClass.jsx
@@ -2,49 +2,90 @@ import React, { Component } from 'react';
 
 class ResponseCheckClass extends Component {
   state = {
-    text: '클릭해서 시작하세요',
-    color: 'skyblue',
-    start: false,
-    response: new Date().getTime(),
+    state: 'waiting',
+    message: '클릭해서 시작하세요.',
+    result: [],
   };
 
+  timeout;
+  startTime;  // 바뀌어도 렌더링 안됨
+  endTime;
+
   onClick = () => {
-    if (!this.state.start) {
+    const { state, message, result } = this.state;
+
+    if (state === 'waiting') {
       this.setState({
-        text: '초록색이 되면 클릭하세요',
-        color: 'red',
-        start: true,
+        state: 'ready',
+        message: '초록색이 되면 클릭하세요',
       });
-  
-      setTimeout(() => {
+
+      this.timeout = setTimeout(() => {
         this.setState({
-          color: 'green',
-          response: new Date().getTime(),
+          state: 'now',
+          message: '지금 클릭',
         });
-      }, 3000);
-    } else {
-      console.log(`반응속도 : ${new Date().getTime() - this.state.response} ms`);
+
+        this.startTime = new Date();
+      }, Math.floor(Math.random() * 1000) + 2000); // 2~3초 랜덤
+    } else if (state === 'ready') { // 성급하게 클릭
+      clearTimeout(this.timeout);
 
       this.setState({
-        text: '클릭해서 시작하세요',
-        color: 'skyblue',
-        start: false,
+        state: 'waiting',
+        message: '너무 성급하시군요! 초록색으로 변경된 후에 클릭하세요.',
+      });
+    } else if (state === 'now') { // 반응속도 체크
+      this.endTime = new Date();
+
+      this.setState((prevState) => {
+        return {
+          state: 'waiting',
+          message: '클릭해서 시작하세요.',
+          result: [...prevState.result, this.endTime - this.startTime],
+        };
       });
     }
+  
+ 
+    // } else {
+    //   console.log(`반응속도 : ${new Date().getTime() - this.state.response} ms`);
+
+    //   this.setState({
+    //     text: '클릭해서 시작하세요',
+    //     color: 'skyblue',
+    //     start: false,
+    //   });
+    // }
+  }
+
+  resultAverage = () => {
+    const { result } = this.state;
+
+    return result.length === 0 ? null : <div>평균 시간: {result.reduce((a, c) => a + c) / result.length} ms</div>;
   }
 
   render() {
+    const { state, message } = this.state;
+
     return (
       <>
-        <div onClick={this.onClick} style={{
-          backgroundColor: `${this.state.color}`, 
-          width: '300px', 
-          height: '300px',
-          textAlign: 'center',
-          padding: '3px',
-        }}>
-          <h2>{this.state.text}</h2>
+        <div
+          id="screen"
+          className={state}
+          onClick={this.onClick}
+        >
+          {message}
         </div>
+        {this.resultAverage()}
+        {/* { 
+          this.state.result.length === 0 ? 
+          null : <div>평균 시간: {this.state.result.reduce((a, c) => a + c) / this.state.result.length} ms</div>
+        }
+        {
+          this.state.result.length !== 0
+          && <div>평균 시간: {this.state.result.reduce((a, c) => a + c) / this.state.result.length} ms</div>
+        } */}
       </>
     );
   };
